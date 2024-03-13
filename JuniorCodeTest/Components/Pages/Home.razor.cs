4f9b1e673f68d0d4407b0552bf6dd35f689011b2
bag2003 <paul_thomas86@hotmail.com>
2024-03-13 14:16:08 +0000

    Completed Task Upload

---
+++ JuniorCodeTest/Components/Pages/Home.razor.cs
@@ -0,0 +1,44 @@
+﻿using JuniorCodeTest.Interfaces;
+using JuniorCodeTest.Models;
+
+using Microsoft.AspNetCore.Components;
+
+
+namespace JuniorCodeTest.Components.Pages
+{
+	public partial class Home : ComponentBase
+	{
+		public List<RequestedUsersModel> RandomUsers { get; set; } = new();
+
+		[Inject]
+		private IRandomUserApiService? RandomUserApiService { get; set; }
+
+
+		protected override async Task OnInitializedAsync()
+		{
+			await base.OnInitializedAsync();
+
+			try
+			{
+				await PopulateUserList();
+			}
+			catch (Exception ex)
+			{
+
+			}
+		}
+
+		private async void RefreshData()
+		{
+			RandomUsers.Clear();
+			await PopulateUserList();
+            StateHasChanged();
+		}
+
+		async Task PopulateUserList()
+		{
+			var users = await RandomUserApiService.GetRandomUserDataFromApi();
+			RandomUsers.AddRange(users);
+		}
+	}
+}
